53000eeaa3a3bcf083df658b83aafe77d798c198
Kenny <kennysalazar3197@gmail.com>
2022-04-05 15:46:58 -0500

    Created Redux function actions for fetching Dragons through API

--- src/redux/dragons/dragon.js
+++ src/redux/dragons/dragon.js
@@ -1,4 +1,9 @@
-const dragonReducer = (state = 'test-dragons', action) => {
+const FETCH_DRAGONS_BEGAN = 'space-travelers-hub/dragons/FETCH_DRAGONS_BEGAN';
+const FETCH_DRAGONS_FAILED = 'space-travelers-hub/dragons/FETCH_DRAGONS_FAILED';
+const FETCH_DRAGONS_SUCCEEDED = 'space-travelers-hub/dragons/FETCH_DRAGONS_SUCCEEDED';
+const DRAGONS_API = 'https://api.spacexdata.com/v3/dragons';
+
+const dragonReducer = (state = {}, action) => {
   switch (action.type) {
     case 'ANY_TYPE':
       return state;
@@ -6,4 +11,38 @@ const dragonReducer = (state = 'test-dragons', action) => {
       return state;
   }
 };
+
+const fetchDragonsBegin = () => (
+  {
+    type: FETCH_DRAGONS_BEGAN,
+  }
+);
+
+const fetchDragonsFailed = (error) => (
+  {
+    type: FETCH_DRAGONS_FAILED,
+    error,
+  }
+);
+
+const fetchDragonsSucess = (data) => (
+  {
+    type: FETCH_DRAGONS_SUCCEEDED,
+    payload: data,
+  }
+);
+
+export const fetchDragonsDate = () => async (dispatch) => {
+  dispatch(fetchDragonsBegin());
+  try {
+    const dragonsResponse = await fetch(DRAGONS_API);
+    if (!dragonsResponse.ok) throw Error(dragonsResponse.statusText);
+    const responseJSON = await dragonsResponse.json();
+
+    dispatch(fetchDragonsSucess(responseJSON));
+  } catch (error) {
+    dispatch(fetchDragonsFailed(error));
+  }
+};
+
 export default dragonReducer;
