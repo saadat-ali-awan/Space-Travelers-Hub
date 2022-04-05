7656cf8b03202b3e603a5355555201df7205f979
Saadat123456 <saadatlucky313@gmail.com>
2022-04-05 20:02:40 +0500

    Create Reducer for Dragons Page

---
+++ src/redux/dragons/dragon.js
@@ -0,0 +1,9 @@
+const dragonReducer = (state, action) => {
+  switch (action.type) {
+    case 'ANY_TYPE':
+      return state;
+    default:
+      return state;
+  }
+};
+export default dragonReducer;
