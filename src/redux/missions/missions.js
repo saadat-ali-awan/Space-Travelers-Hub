f4d8d2a84f5f4832ec60a8c86290da26959fbb12
Ntare Guy <gntare2@gmail.com>
2022-04-06 01:48:45 +0200

    Added mission action creator

--- src/redux/missions/missions.js
+++ src/redux/missions/missions.js
@@ -2,6 +2,25 @@ const initialState = [];
 
 const GET_DATA_MISSIONS = 'GET_DATA_MISSIONS';
 
+export const getMissionData = () => async (dispatch) => {
+  const response = await fetch('https://api.spacexdata.com/v3/missions', {
+    method: 'GET',
+  });
+
+  const responseJSON = await response.json();
+  const data = responseJSON.map((mission) => (
+    {
+      id: mission.mission_id,
+      name: mission.mission_name,
+      description: mission.description,
+    }
+  ));
+  dispatch({
+    type: GET_DATA_MISSIONS,
+    data,
+  });
+};
+
 const MissionReducer = (state = initialState, action) => {
   switch (action.type) {
     case GET_DATA_MISSIONS:
