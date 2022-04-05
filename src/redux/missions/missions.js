b3bdcbdb7f990cae142c9bbd492ccf24f9d0f078
Saadat123456 <saadatlucky313@gmail.com>
2022-04-05 20:02:22 +0500

    Create Reducer for Missions Page

---
+++ src/redux/missions/missions.js
@@ -0,0 +1,9 @@
+const missonsReducer = (state, action) => {
+  switch (action.type) {
+    case 'ANY_TYPE':
+      return state;
+    default:
+      return state;
+  }
+};
+export default missonsReducer;
