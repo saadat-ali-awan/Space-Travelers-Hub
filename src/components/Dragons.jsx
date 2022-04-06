e8a401c8dc794a2a7a360d64bf792617f45fef9c
Kenny <kennysalazar3197@gmail.com>
2022-04-06 11:00:57 -0500

    Updated Dragons component for rendering the list of dragons missions

--- src/components/Dragons.jsx
+++ src/components/Dragons.jsx
@@ -1,13 +1,38 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import Dragon from './Dragon';
 
 const Dragons = () => {
   const dragonsList = useSelector((state) => state.dragonReducer);
 
+  const renderState = () => {
+    switch (dragonsList.status) {
+      case 'FETCHING_DRAGONS':
+        return <li>LOADING...</li>;
+      case 'FETCHING_FAILED':
+        return <li>{`Something went wrong: ${dragonsList.error}`}</li>;
+      case 'FETCHING_SUCCEEDED':
+        return dragonsList.dragons.map((item) => (
+          <li key={item.id}>
+            <Dragon
+              id={item.id}
+              name={item.name}
+              type={item.type}
+              image={item.flickr_images}
+            />
+          </li>
+        ));
+      default:
+        return <li>Unexpected behavior</li>;
+    }
+  };
+
   return (
     <div>
       <section>
-        {`Dragons page ${dragonsList}`}
+        <ul>
+          {renderState()}
+        </ul>
       </section>
     </div>
   );
