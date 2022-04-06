29c205cc82a51c4b78e8560f6d9e0565227ce77d
Saadat123456 <saadatlucky313@gmail.com>
2022-04-06 09:03:58 +0500

    Create Layout of Rockets Page

--- src/components/rockets/rockets.jsx
+++ src/components/rockets/rockets.jsx
@@ -1,10 +1,25 @@
-import { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import styles from './rockets.module.css';
 
 const rockets = () => {
-  useEffect(() => {
-  }, []);
+  const rocketsList = useSelector((state) => state.rocketsReducer);
   return (
-    <div>Rockets</div>
+    <main className={styles['rockets-container']}>
+      {
+        rocketsList[0].map((rocket) => (
+          <div key={`${rocket.key}-item`} className={styles['rocket-details']}>
+            <div>
+              <img src={`${rocket.flickr_images[0]}`} alt="Rocket" className={styles['rocket-img']} />
+            </div>
+            <div>
+              <h3 className={styles['rocket-heading']}>{rocket.name}</h3>
+              <p className={styles['rocket-description']}>{rocket.description}</p>
+              <button className={styles['rocket-button']} type="button">Reserve Rocket</button>
+            </div>
+          </div>
+        ))
+      }
+    </main>
   );
 };
 
