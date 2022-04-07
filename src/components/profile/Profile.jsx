4c171d0ee87cc93f0f9d2e74a2fd7f8ce16957a8
Saadat123456 <saadatlucky313@gmail.com>
2022-04-08 00:47:00 +0500

    Add Reserved Rockets to Profile Page

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,5 +1,22 @@
+import { useSelector } from 'react-redux';
 import styles from './Profile.module.css';
 
-const Profile = () => (<div className={styles.default}>My Profile Page</div>);
+const Profile = () => {
+  const rockets = useSelector((state) => state.rocketsReducer);
+  return (
+    <div className={styles.default}>
+      <div>
+        <h3>My Rockets</h3>
+        <ul className={styles.rockets}>
+          {
+            rockets.filter((rocket) => rocket.reserved).map((rocket) => (
+              <li key={rocket.key} className={styles.listItem}>{rocket.name}</li>
+            ))
+          }
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 export default Profile;
