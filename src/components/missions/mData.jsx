1063f638c6ba6c53c83dfe1b075d21aca3402f62
Ntare Guy <gntare2@gmail.com>
2022-04-07 21:01:29 +0200

    Fix member page

--- src/components/missions/mData.jsx
+++ src/components/missions/mData.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import MissionButton from './missionButton';
-import Member from './Member';
+import Member from './member';
 
 const MissionData = (props) => {
   const { mission } = props;
